94295719c253adafbe9602f1cad76976d03ca53e
saurabhs <saurabhpshirsath@gmail.com>
2021-09-15 10:24:15 +0530

    [saurabh] Add:Function Problems

---
+++ FunctionProblem/DegreeConversion.js
@@ -0,0 +1,12 @@
+function DegCToDegF(value)
+{
+    celcius=(value * ( 9/5 )+ 32 );
+    console.log("Deg C => Deg F :"+celcius);
+}
+function DegFToDegC(val)
+{
+    Farenheit=(val-32) * ( 5/9 )
+    console.log("Deg F => Deg C :"+Farenheit);
+}
+DegCToDegF(45);
+DegFToDegC(45);
